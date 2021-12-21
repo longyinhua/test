6dc384b5c594115c077f3d2264d177331b1ac070
lyh <2042064113@qq.com>
2021-12-21 17:53:12 +0800

    sql 使用注解

--- src/main/java/com/example/test/userInfo/mapper/UserinfoMapper.java
+++ src/main/java/com/example/test/userInfo/mapper/UserinfoMapper.java
@@ -2,6 +2,9 @@ package com.example.test.userInfo.mapper;
 
 import com.example.test.userInfo.model.Userinfo;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
@@ -15,6 +18,11 @@ import java.util.List;
  */
 public interface UserinfoMapper extends BaseMapper<Userinfo> {
 
+     @Select("select * from userinfo")
+     @Results(id="studentMap",value = {
+             @Result(id = true, column = "id", property = "id"),
+             @Result(column = "name", property = "name"),
+             @Result(column = "password", property = "password"),})
      List<Userinfo> findAllUser();
 
      Userinfo getUser(String userName , String password);
